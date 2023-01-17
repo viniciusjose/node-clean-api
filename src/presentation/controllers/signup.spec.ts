84ab08c2325208669460f73d7f094e57d711c042
Vinicius Silva <vinicius.silva@telium.com.br>
2023-01-17 18:08:28 -0300

    test: ensure signUpController return 400 if no name provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', function () {
+  it('should be return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpReponse = sut.handle(httpRequest)
+    expect(httpReponse.statusCode).toBe(400)
+  })
+})
