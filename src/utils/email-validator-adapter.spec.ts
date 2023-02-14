5ae44317915e8e100ea1264a282b64a27a9f31f9
Vinicius Silva <vinicius.silva@telium.com.br>
2023-02-14 11:31:23 -0300

    feat: create EmailValidatorAdapter

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,10 +1,37 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
+
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
 
 describe('EmailValidator Adapter', function () {
   it('should return false if validator returns false', function () {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
+  })
+
+  it('should return true if validator returns true', function () {
+    const sut = makeSut()
+    const isValid = sut.isValid('invalid@mail.com')
+
+    expect(isValid).toBe(true)
+  })
+
+  it('should call validator with correct email', function () {
+    const sut = makeSut()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@mail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
   })
 })
